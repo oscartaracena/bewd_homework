35f0c983eb345694f7306fe69073633bea180728
oscar.taracena@gmail.com <oscar.taracena@gmail.com>
2015-03-28 16:13:44 -0700

    Simple ruby file that asks some questions like name, where I am from, job, and talent. I use gets.chomp() for user input, and string function .downcase

---
+++ whoami.rb
@@ -0,0 +1,9 @@
+puts "What is your name?"
+name = gets.chomp
+puts "where did you grow up?"
+city = gets.chomp
+puts "What do you for a living?"
+job = gets.chomp
+puts "what is your hidden talent?"
+talent = gets.chomp
+puts "My name is: " + name.downcase + " I grew up in " + city + " you work as a " + job + " your hidden talent is " + talent + "."
